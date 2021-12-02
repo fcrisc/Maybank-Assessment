7d73a9c3eef8a53693a88d1979fd0d568aa8d32f
Muhammad Faris <miitfaris@gmail.com>
2021-12-02 15:50:10 +0800

    Api Test

---
+++ api/views.py
@@ -0,0 +1,31 @@
+from django.http.response import JsonResponse
+from django.shortcuts import render
+from geopy.geocoders import Nominatim
+from datetime import datetime
+import pytz
+
+
+import requests
+
+# Create your views here.
+
+def apiOverview(request):
+    response = requests.get('http://api.open-notify.org/iss-now.json')
+    tz = pytz.timezone("Asia/Kuala_Lumpur")
+    issdata = response.json()
+    
+    iss_datetime = datetime.fromtimestamp(issdata['timestamp'], tz)
+    geolocator = Nominatim(user_agent="geoapiExercises")
+    latitude = str(issdata['iss_position']['latitude'])
+    longitude = str(issdata['iss_position']['longitude'])
+    
+    location = geolocator.reverse(latitude+","+longitude)
+    
+    context = {
+        'iss_datetime': iss_datetime,
+        'latitude': latitude,
+        'longitude': longitude,
+        'location': location,
+        }
+    
+    return render(request, 'api/iss-location.html', context)
